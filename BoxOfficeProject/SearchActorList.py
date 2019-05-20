349c57ab1ab49e7290183371f4927b72894ff46e
Kim Ha Yoon <ekgp6360@naver.com>
2019-05-20 15:11:20 +0900

    190520

--- BoxOfficeProject/SearchActorList.py
+++ BoxOfficeProject/SearchActorList.py
@@ -1,6 +1,5 @@
-# 리스트 박스에서 배우를 클릭하면 엔트리에 그 이름이 들어가고 검색버튼을 누르면
+# 리스트 박스에서 배우를 클릭하면
 # 오른쪽에 프로필사진이나 이미지를 띄우고 관련된 뉴스 기사를 띄우기
-
 
 
 from tkinter import *
@@ -17,20 +16,41 @@ class ActorList():
         self.SearchFrame = Frame(self.Background, width = self.width / 2, height = 100, bg = "light blue")
         self.SearchFrameLabel = Label(self.SearchFrame, font=("나눔 고딕", 25, "bold"), text="배우 상세 정보", bg = "light blue")
         self.SearchEntry = Entry(self.SearchFrame, width = 31, font=("HYHeadLine", 15, "bold"), bd = 6, relief = "ridge")
-        self.SearchBnt = Button(self.SearchFrame, text = "검색",   font=("HYHeadLine", 14, "bold"), width = 6, bd = 3, command = self.Search)
+        self.SearchBnt = Button(self.SearchFrame, text = "검색",   font=("나눔 고딕", 14, "bold"), width = 6, bd = 3, command = self.Search)
 
         self.ListFrame = Frame(self.Background, width=self.width / 2, height = 370, bg = "light blue")
         self.ActorListBox = Listbox(self.ListFrame, width = 46, height = 22, bd = 6, relief="ridge")
         self.ActorListScrollbar = Scrollbar(self.ListFrame)
         self.ActorListScrollbar["command"] = self.ActorListBox.yview
-        self.ActorListData = LoadXMLFromFileActorList(1, None, None)        # page = 1, name 이랑 movie를 none으로 인자를 넣으면 전체목록을 부름
+        self.NextBnt = Button(self.ListFrame, font=("HYHeadLine", 10, "bold"), text="다음 페이지", bd=3, width=9, command=self.Next)
+        self.PrevBnt = Button(self.ListFrame, font=("HYHeadLine", 10, "bold"), text="이전 페이지", bd=3, width=9, command=self.Prev)
+
+        self.ListPage = 1
+        self.TotalPage = 1
+        self.TotalPage = self.GetTotalPage()
+        #print(self.TotalPage)
+        #self.InsertTotalData()
+        self.ActorListData = LoadXMLFromFileActorList(self.ListPage, None, None)        # page = 1, name 이랑 movie를 none으로 인자를 넣으면 전체목록을 부름
         self.InsertActorList()
 
         self.InfoFrame = Frame(self.Background, width = self.width / 2, height = self.height, bg = "light blue", bd = 6, relief = "ridge")
         #self.InfoCanvas = Canvas(self.InfoFrame, width=self.width / 2 - 10, height=500, bd=4, relief="ridge", bg="light blue")
 
+    def InsertTotalData(self):
+        for i in range(self.TotalPage):
+            self.ActorListData = LoadXMLFromFileActorList(i, None, None)  # page = 1, name 이랑 movie를 none으로 인자를 넣으면 전체목록을 부름
+            self.InsertActorList()
 
+    def GetTotalPage(self):
+        page = 177981 // 50
+        if (177981 % 50) > 0:
+            page += 1
 
+        return page
+
+    def ActorPersons(self):
+        self. Persons = self.ActorListData.find_all("totCnt")
+        print(self.Persons)
 
     def InsertActorList(self):
         i = 0  # 리스트박스는 배열처럼 쓰기 때문에 일종의 인덱스 표현을 위해서
@@ -43,16 +63,25 @@ class ActorList():
     def Search(self):
         pass
 
+    def Next(self):
+        self.ListPage += 1
+
+    def Prev(self):
+        pass
+
     def Render(self):
         self.InfoFrame.pack(side = RIGHT, expand=True)
         self.SearchFrame.pack(anchor = "nw", expand=True)
-        self.ListFrame.pack(anchor = "w",expand=True)
+        self.ListFrame.pack(anchor = "w",expand=True,fill="both")
 
         self.SearchFrameLabel.place(x = 10, y = 10)
         self.SearchEntry.place(x=0, y=62)
         self.SearchBnt.place(x = 355, y = 60)
-        self.ActorListScrollbar.pack(side=RIGHT, fill="y")
-        self.ActorListBox.pack(anchor="s")
+
+        self.ActorListBox.pack(side = LEFT)
+        self.ActorListScrollbar.pack(side=LEFT, fill="y")
+        self.NextBnt.place(x = 355, y = 10)
+        self.PrevBnt.place(x = 355, y = 40)             # 버튼 간격 30씩
 
         #self.InfoCanvas.place(x = 0, y = 0)
 
